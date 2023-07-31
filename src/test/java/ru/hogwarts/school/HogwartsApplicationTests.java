7e1047a1aec22ccd21cf0564fd198f88f10614c1
Pirania69 <kanashevsky@mail.ru>
2023-08-01 00:35:52 +0300

    DZ_test1

--- src/test/java/ru/hogwarts/school/HogwartsApplicationTests.java
+++ src/test/java/ru/hogwarts/school/HogwartsApplicationTests.java
@@ -1,13 +1,46 @@
 package ru.hogwarts.school;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import ru.hogwarts.school.controllers.StudentController;
+import ru.hogwarts.school.model.Student;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HogwartsApplicationTests {
+    @LocalServerPort
+    private int port;
+    @Autowired
+    private StudentController studentController;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() throws Exception{
+        Assertions.assertThat(studentController).isNotNull();
+    }
+    @Test
+    public void findStudents() throws Exception{
+        Assertions
+                .assertThat(this.restTemplate.getForObject("http://localhost:"+port+"/student/all",String.class))
+                .isNotNull();
+    }
+    @Test
+    public void findStudentById() throws Exception{
+        Assertions
+                .assertThat(this.restTemplate.getForObject("http://localhost:"+port+"/student/1",String.class))
+                .isNotNull();
+    }
+    @Test
+    public void findStudentsByIdFaculty() throws Exception{
+        Assertions
+                .assertThat(this.restTemplate.getForObject("http://localhost:"+port+"/student/2/faculty",Object.class))
+                .isNotNull();
     }
 
 }
