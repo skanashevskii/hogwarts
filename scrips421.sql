99589991bed7c049a90e1d86d2fc47944ea3a887
Pirania69 <kanashevsky@mail.ru>
2023-08-12 12:00:06 +0300

    ДЗ SQL Next

--- scrips421.sql
+++ scrips421.sql
@@ -2,14 +2,16 @@
 ALTER TABLE student
 ADD CONSTRAINT check_age CHECK (age >=16);
 
-ALTER TABLE student
-ADD COLUMN name_unique UNIQUE (name);
+ALTER TABLE IF EXISTS people
+ADD CONSTRAINT name_unique UNIQUE(name);
 
-ALTER TABLE faculty
-ADD CONSTRAINT faculty_color_unique UNIQUE(name,color);
+    ALTER TABLE IF EXISTS faculty
+    ADD CONSTRAINT faculty_color_unique UNIQUE(name,color);
+
+
 
 ALTER TABLE student
-ALTER COLUMN age SET DEFAULT 20;
+ADD COLUMN age SET DEFAULT 20;
 
 
 
